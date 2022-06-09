07df0861ea1855405c2931b6438f861487a23226
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-06-09 17:22:31 -0600

    Made the "Homepage" component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
+import Homepage from './Components/Homepage/Homepage';
 
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <Navbar />
+      <div className="app__navbar-homepage">
+        <Navbar />
+        <Homepage />
+      </div>
     </div>
   )
 }
---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Homepage.css";
+
+export default function Homepage() {
+  return (
+    <main className="homepage">
+      <h1 className="homepage__title">Hi I'm</h1>
+      <h2 className="homepage__name">Braden</h2>
+      <h2 className="homepage__position">Front End Developer</h2>
+    </main>
+  );
+}

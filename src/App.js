649a71177e2bda44024beb07dc100e188ef899c0
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-06-10 16:41:58 -0600

    Created and made basic desgin for "Project" component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React from 'react';
-import Navbar from './Components/Navbar/Navbar';
-import Homepage from './Components/Homepage/Homepage';
-import About from './Components/About/About'
-import Skills from './Components/Skills/Skills';
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Homepage from "./Components/Homepage/Homepage";
+import About from "./Components/About/About";
+import Skills from "./Components/Skills/Skills";
+import Project from "./Components/Project/Project";
 
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="app">
-      <div className="app__navbar-homepage">
-        <Navbar />
-        <Homepage />
-      </div>
-      <About />
-      <Skills />
-    </div>
-  )
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div className="app">
+            <div className="app__navbar-homepage">
+              <Navbar />
+              <Homepage />
+            </div>
+            <About />
+            <Skills />
+            <Project />
+          </div>
+        }
+      />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/Components/Project/Project.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "./Project.css";
+
+export default function Project() {
+  return (
+    <div className="project">
+      <h2 className="project__title">Projects</h2>
+      <div className="project__grid">
+        <div className="project__grid__beer">
+          <h3 className="project__grid__beer-title">Beer Company Website</h3>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://braden0006.github.io/beer-company-website/"
+          >
+            <img
+              className="project__grid__beer-image"
+              src="Images/beer-company.png"
+              alt=""
+            />
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Braden0006/beer-company-website"
+          >
+            <button className="project__grid__beer-code">See Live Code</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}

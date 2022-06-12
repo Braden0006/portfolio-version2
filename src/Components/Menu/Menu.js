f45e0ad7f072f67d0b1cfb5f8fa05462d634cc48
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-06-12 16:26:17 -0600

    Links of the menu stagger down when opened

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -6,13 +6,38 @@ import { AiOutlineClose } from "react-icons/ai";
 import "./Menu.css";
 
 export default function Menu({ toggleOpen, open }) {
+  const variants = {
+    hidden: {
+      y: -1000,
+    },
+    show: {
+      y: 0,
+      transition: {
+        staggerChildren: 0.2,
+        type: "tween",
+      },
+    },
+  };
+
+  const item = {
+    hidden: {
+      opacity: 0,
+    },
+    show: {
+      opacity: 1,
+      transition: {
+        duration: 0.8,
+      },
+    },
+  };
+
   return (
     <>
       <motion.div
         className="menu-background"
-        initial={{ y: -1000 }}
-        animate={{ y: open ? 0 : -1000 }}
-        transition={{type: "tween"}}
+        variants={variants}
+        initial="hidden"
+        animate={open ? "show" : "hidden"}
       >
         <span className="menu__icon-container">
           <AiOutlineClose
@@ -21,12 +46,25 @@ export default function Menu({ toggleOpen, open }) {
           />
         </span>
         <div className="menu__container">
-          <ul className="menu__list">
-            <li className="menu__list-item">About</li>
-            <li className="menu__list-item">Skills</li>
-            <li className="menu__list-item">Projects</li>
-            <li className="menu__list-item">Contact</li>
-          </ul>
+          <motion.ul
+            className="menu__list"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: open ? 1 : 0 }}
+            transition={{ delay: 0.3, delayChildren: 0.5, staggerChildren: 1 }}
+          >
+            <motion.li variants={item} className="menu__list-item">
+              About
+            </motion.li>
+            <motion.li variants={item} className="menu__list-item">
+              Skills
+            </motion.li>
+            <motion.li variants={item} className="menu__list-item">
+              Projects
+            </motion.li>
+            <motion.li variants={item} className="menu__list-item">
+              Contact
+            </motion.li>
+          </motion.ul>
         </div>
       </motion.div>
     </>

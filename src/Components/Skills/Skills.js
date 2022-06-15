09c792b53d2d6269b23feee5b743616f3dd97008
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-06-15 16:57:55 -0600

    Changed the design of the title of the "Skills" component

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -5,7 +5,9 @@ import "./Skills.css";
 export default function Skills() {
   return (
     <div className="skills" id="skills">
-      <h2 className="skills__title">Skills</h2>
+      <div className="skills__title-container">
+        <h2 className="skills__title-container__title">Skills</h2>
+      </div>
       <div className="skills__grid">
         <img className="skills__grid-icon" src="Images/html-5.png" alt="" />
         <img className="skills__grid-icon" src="Images/css-3.png" alt="" />

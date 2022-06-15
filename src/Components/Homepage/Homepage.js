767abea35546fdcd7f07b57396805cc5802ea8f1
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-06-15 16:46:41 -0600

    Made some styling changes to "About" component

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -4,7 +4,7 @@ import "./Homepage.css";
 export default function Homepage() {
   return (
     <main className="homepage">
-      <h1 className="homepage__title">Hi I'm</h1>
+      <h1 className="homepage__title">Hello I'm</h1>
       <h2 className="homepage__name">Braden</h2>
       <h2 className="homepage__position">Front End Developer</h2>
     </main>

7b5296b178c1980f24e968cf80a06f4be78fc16d
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-07-27 17:12:45 -0600

    Changes some designs in "About"

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,7 +4,10 @@ import "./About.css";
 export default function About() {
   return (
     <div className="about" id="about">
-      <h2 className="about__title">About me</h2>
+      <div className="about__title-container">
+        <h2 className="about__title">About me</h2>
+        <span><hr /></span>
+      </div>
       <div className="about__info-container">
         <p className="about__info-container__description">
           I'm an aspiring Front End Developer and have been studying HTML, CSS,

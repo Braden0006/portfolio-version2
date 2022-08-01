3d1fc31b93951dbdb0260aa0789a9221ef04f48f
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-08-01 10:38:58 -0600

    "Footer" is resonsive on all devices

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { HashLink } from "react-router-hash-link";
+
 import { FaGithubSquare } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 
@@ -8,11 +10,27 @@ export default function Footer() {
   return (
     <div className="footer-container">
       <div className="footer">
-          <span className="footer__logo">BW</span>
-          <div className="footer__social">
+        <span className="footer__logo">
+          <HashLink className="footer__logo__link" to="#home" smooth="true">
+            BW
+          </HashLink>
+        </span>
+        <div className="footer__social">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Braden0006"
+          >
             <FaGithubSquare className="footer__social__icon" />
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/braden-whitcher-814b23212/"
+          >
             <FaLinkedin className="footer__social__icon" />
-          </div>
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -3,7 +3,7 @@ import "./Homepage.css";
 
 export default function Homepage() {
   return (
-    <main className="homepage">
+    <main className="homepage" id="home">
       <div className="homepage__info">
         <h1 className="homepage__info__title">Hi, my name is</h1>
         <h2 className="homepage__info__name">Braden</h2>

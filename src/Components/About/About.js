524cd427e99a8aa273d8b9ba398497fec07f2dc3
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-08-02 14:56:49 -0600

    Fixed animation delay in "About"

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -12,8 +12,8 @@ export default function About() {
     onscreen: {
       opacity: 1,
       transition: {
-        duration: 0.3,
-        delay: 0.5,
+        duration: 0.5,
+        delay: 0.3,
       },
     },
   };

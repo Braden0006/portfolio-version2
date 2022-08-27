facd5d0e4cd9eb1bdd8a655cf9dffa1007b0b331
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-08-26 19:05:39 -0600

    Made some layout changes for desktop in "Project"

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -143,11 +143,55 @@ export default function Project() {
             <a
               target="_blank"
               rel="noreferrer"
-              href="https://braden0006.github.io/coffeeroasters-subscription-site/"
+              href="https://coffeeroasters-subscription-website.netlify.app/"
             >
               <h3 className="project__grid__site__overlay__title">
                 Coffee Roasters
               </h3>
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/Braden0006/coffeeroasters-subscription-site"
+            >
+              <button className="project__grid__site__overlay__code">
+                See Live Code
+              </button>
+            </a>
+          </div>
+        </div>
+
+        <div className="project__grid__site">
+          {/* <img
+            className="project__grid__site__image"
+            sizes="(max-width: 1400px) 100vw, 1400px"
+            srcSet="
+              Images/coffee-roasters_xctnl1_c_scale,w_190.png 190w,
+              Images/coffee-roasters_xctnl1_c_scale,w_638.png 638w,
+              Images/coffee-roasters_xctnl1_c_scale,w_931.png 931w,
+              Images/coffee-roasters_xctnl1_c_scale,w_1189.png 1189w,
+              Images/coffee-roasters_xctnl1_c_scale,w_1400.png 1400w"
+            src="Images/coffee-roasters_xctnl1_c_scale,w_1400.png"
+            alt="the home page of a coffee subscription website"
+          ></img> */}
+
+          <img
+            className="project__grid__site__image"
+            sizes="(max-width: 1400px) 100vw, 1400px"
+            srcset="
+              Images/Screen_Shot_2022-08-26_at_6.22.53_PM_cbqw1l_c_scale,w_200.png 200w,
+              Images/Screen_Shot_2022-08-26_at_6.22.53_PM_cbqw1l_c_scale,w_1400.png 1400w"
+            src="Screen_Shot_2022-08-26_at_6.22.53_PM_cbqw1l_c_scale,w_1400.png"
+            alt=""
+          ></img>
+
+          <div className="project__grid__site__overlay">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://my-team-website.netlify.app/"
+            >
+              <h3 className="project__grid__site__overlay__title">myteam</h3>
             </a>
             <a
               target="_blank"
